6d494c37f2348475d3bec99f8f3e954db6eff68c
Tim Murray <timmurray@google.com>
2019-04-29 08:52:42 -0700

    Revert "time: sched_clock: record cycle count in suspend and resume"
    This reverts commit 71dddbcc4621693e863b8cd4e2acc3ce187e80ed.
    
    bug 131260677
    
    Change-Id: Iec02f0b15109f6de36e1e5bca8edf60b615d5897
    Signed-off-by: Tim Murray <timmurray@google.com>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- kernel/time/sched_clock.c
+++ kernel/time/sched_clock.c
@@ -71,10 +71,6 @@ struct clock_data {
 
 static struct hrtimer sched_clock_timer;
 static int irqtime = -1;
-static u64 suspend_ns;
-static u64 suspend_cycles;
-static u64 resume_cycles;
-static u64 resume_ns;
 core_param(irqtime, irqtime, int, 0400);
 //POWER ADD
 static u64 first_cycle = 1;
@@ -307,26 +303,6 @@ int sched_clock_suspend(void)
 	struct clock_read_data *rd = &cd.read_data[0];
 
 	update_sched_clock();
-
-	suspend_ns = rd->epoch_ns;
-	suspend_cycles = rd->epoch_cyc;
-	pr_info("suspend ns:%17llu	suspend cycles:%17llu\n",
-				rd->epoch_ns, rd->epoch_cyc);
-//POWER ADD
-	if (first_cycle) {
-		last_wake_time = 0;
-		sum_wakeup_time = 0;
-		sum_wakeup_times = 0;
-		first_cycle = 0;
-	} else if (off_flag) {
-		off_flag = false;
-		last_wake_time = abs(suspend_ns-off_resume_ns)/1000000;
-		sum_wakeup_time += last_wake_time;
-	} else {
-		sum_wakeup_times += 1;
-		last_wake_time = abs(suspend_ns-resume_ns)/1000000;
-		sum_wakeup_time += last_wake_time;
-	}
 	hrtimer_cancel(&sched_clock_timer);
 	rd->read_sched_clock = suspended_sched_clock_read;
 
@@ -338,11 +314,6 @@ void sched_clock_resume(void)
 	struct clock_read_data *rd = &cd.read_data[0];
 
 	update_sched_clock();
-
-	resume_ns = rd->epoch_ns;
-	//rd->epoch_cyc = cd.actual_read_sched_clock();
-	resume_cycles = rd->epoch_cyc;
-	pr_info("resume cycles:%17llu\n", rd->epoch_cyc);
 
 	hrtimer_start(&sched_clock_timer, cd.wrap_kt, HRTIMER_MODE_REL);
 	rd->read_sched_clock = cd.actual_read_sched_clock;
